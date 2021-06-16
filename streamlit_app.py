d454c68b37dcc0d17a7d79c2a5c7d362d4644f38
Marcela Cruz Félix <marce.crfx@gmail.com>
2021-06-15 22:12:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,19 @@ import numpy as np
 import altair as alt
 import pydeck as pdk
 
-st.write("wharap")
+st.write("wharap1")
 
 
+DATA_URL = (
+    "https://storage.googleapis.com/hdsp-data/CLUESBC.csv"
+)
+
+@st.cache(persist=True)
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    return data
 
 # CREATING FUNCTION FOR MAPS
 
