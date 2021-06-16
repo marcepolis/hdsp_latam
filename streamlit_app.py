d5c0746def924e9a8ff07522931a833aaf7b9ec9
Marcela Cruz Félix <marce.crfx@gmail.com>
2021-06-15 19:24:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,16 @@
 import streamlit as st
+import pandas as pd
+
+
 st.write('Hello, world!')
 
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
 
-import pandas as pd
+st.write('This is a test')
 
-death_db = st.file_uploader("Pick a file")
-
-death_db = pd.read_csv(death_db)
+death_db = pd.read_csv(file_uploader)
 
 print('observations {0} and attributes {1}'.format(death_db.shape[0], (death_db.shape[1])))
+
 
