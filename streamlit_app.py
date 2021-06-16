93cf3588dbdcd9eb650c76f227d2db558927af3c
Marcela Cruz Félix <marce.crfx@gmail.com>
2021-06-15 22:16:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,12 @@ import numpy as np
 import altair as alt
 import pydeck as pdk
 
-st.write("wharap1")
+st.write("wharap2")
 
 
-DATA_URL = (
-    "https://storage.googleapis.com/hdsp-data/CLUESBC.csv"
-)
+DATA_URL = ("https://storage.googleapis.com/hdsp-data/CLUESBC.csv")
 
-data = pd.read_csv(DATA_URL, nrows=nrows)
+data = pd.read_csv(DATA_URL)
 
 
 # CREATING FUNCTION FOR MAPS
@@ -42,4 +40,4 @@ def map(data, lat, lon, zoom):
 
 
 st.write("CLUES")
-map(data, zoom_level)
+map(data)
