92f02bd3493e0202f4d350303b408b7840fce2df
Marcela Cruz Félix <marce.crfx@gmail.com>
2021-06-15 19:12:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 death_db = st.file_uploader("Pick a file")
 
-
+death_db = pd.read_csv(death_db)
 
 print('observations {0} and attributes {1}'.format(death_db.shape[0], (death_db.shape[1])))
 
