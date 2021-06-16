b7f98453c75a40ac388ad2154a115387a8e614a6
Marcela Cruz Félix <marce.crfx@gmail.com>
2021-06-15 19:27:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,9 @@ st.write(x, 'squared is', x * x)
 
 st.write('This is a test')
 
-death_db = pd.read_csv(file_uploader)
+dbase = st.file_uploader("Pick a file")
+
+death_db = pd.read_csv('dbase')
 
 print('observations {0} and attributes {1}'.format(death_db.shape[0], (death_db.shape[1])))
 
