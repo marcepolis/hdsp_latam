03b9b237580e964ec3de4b3228e6bd6a6c114df7
Marcela Cruz Félix <marce.crfx@gmail.com>
2021-06-15 19:36:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import altair as alt
+import pydeck as pdk
 
 
 st.write('Hello, world!')
@@ -13,5 +16,27 @@ dbase = st.file_uploader("Pick a file")
 
 st.success('Success message')
 
+def map(data, lat, lon, zoom):
+    st.write(pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": lat,
+            "longitude": lon,
+            "zoom": zoom,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=data,
+                get_position=["lon", "lat"],
+                radius=100,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                pickable=True,
+                extruded=True,
+            ),
+        ]
+    ))
 
 
